ab1da5f7ed7ee4856ddb13a5f70f6790d379450c
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-07-03 16:38:17 +0300

    fix bug with a lot of divs

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,18 +3,16 @@ import {Button, Modal, ModalHeader, ModalFooter} from 'reactstrap'
 
 const ModalPopup = ({toggle, isOpen, onConfirm, title}) => {
     return (
-        <div>
-            <Modal isOpen={isOpen} toggle={toggle}>
-                <ModalHeader toggle={toggle}>{title}</ModalHeader>
-                <ModalFooter>
-                    <Button color="primary" onClick={() => {
-                        onConfirm();
-                        toggle()
-                    }}>Yes</Button>{' '}
-                    <Button color="secondary" onClick={toggle}>Cancel</Button>
-                </ModalFooter>
-            </Modal>
-        </div>
+        <Modal isOpen={isOpen} toggle={toggle}>
+            <ModalHeader toggle={toggle}>{title}</ModalHeader>
+            <ModalFooter>
+                <Button color="primary" onClick={() => {
+                    onConfirm();
+                    toggle()
+                }}>Yes</Button>{' '}
+                <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+        </Modal>
     );
 };
 
