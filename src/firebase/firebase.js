efe0bf8adff4942b637c92ebbc1f75bcff3128cc
Maxym Dyachenko <max.dyachenko@inbox.ru>
2018-07-15 14:24:58 +0300

    add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyDZqvH4QaE0IectAXnHVZeJQ4q0EYKVSz8",
+    authDomain: "birthapp-2af38.firebaseapp.com",
+    databaseURL: "https://birthapp-2af38.firebaseio.com",
+    projectId: "birthapp-2af38",
+    storageBucket: "",
+    messagingSenderId: "939492774035"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
